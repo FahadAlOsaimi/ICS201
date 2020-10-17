f42ed9913c0e33c0dbb06d640ca6ad33a487de48
FahadMesferS <67862836+FahadMesferS@users.noreply.github.com>
2020-10-17 11:32:27 +0300

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,21 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class demo {
     public static void main(String[] args) {
         double s1, s2, s3;
         String color = "";
         boolean filled = false;
-        System.out.print("Please enter three sides of triangle, a color and whether it is filled or not:");
-        Scanner kb = new Scanner(System.in);
-        s1 = kb.nextDouble();
-        s2 = kb.nextDouble();
-        s3 = kb.nextDouble();
-        color = kb.next();
-        filled = kb.nextBoolean();
-        kb.close();
-
-        Triangle obj = new Triangle(s1, s2, s3, color, filled);
+        Scanner kb;
         try {
+            System.out.print("Please enter three sides of triangle, a color and whether it is filled or not:");
+            kb = new Scanner(System.in);
+            s1 = kb.nextDouble();
+            s2 = kb.nextDouble();
+            s3 = kb.nextDouble();
+            color = kb.next();
+            filled = kb.nextBoolean();
+            kb.close();
+            Triangle obj = new Triangle(s1, s2, s3, color, filled);
+
             if (((s1 + s2) <= s3) || ((s3 + s2) <= s1) || ((s3 + s1) <= s2))
                 throw new IllegalTriangleException();
             else {
@@ -27,6 +28,8 @@ public class demo {
 
         } catch (IllegalTriangleException e) {
             System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter in form of: side1 side2 side3 color (true/false)");
         }
     }
 }
